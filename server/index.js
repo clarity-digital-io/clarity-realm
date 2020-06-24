b0836b3c602e0ba6ce4bdf3a626946af3b063c22
Ben Efigenio <befigenio@gmail.com>
2020-06-24 16:05:46 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const SERVER_URL = 'https://forms-dev.us1a.cloud.realm.io';
 const REALM_URL = 'realms://forms-dev.us1a.cloud.realm.io';
 
 const listener = async () => {
-
+	console.log('listener');
 	try {
 		const adminUser = await Realm.Sync.User.login(SERVER_URL, Realm.Sync.Credentials.nickname('realm-admin', true));
 		const config = { serverUrl: REALM_URL, adminUser: adminUser, filterRegex: '/.*/' }
@@ -17,6 +17,7 @@ const listener = async () => {
 const handleChange = async function (changeEvent) {
 
 	var matches = changeEvent.path.match("^/([^/]+)/([^/]+)$");
+	console.log('matches', matches);
 
 	if(changeEvent.changes.hasOwnProperty('Response__c')) {
 		if(changeEvent.changes.Response__c.hasOwnProperty('oldModifications')) {
