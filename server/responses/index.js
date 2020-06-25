7eaead71c2faa34eaefc1d13a9778c8bc81e9845
Ben Efigenio <befigenio@gmail.com>
2020-06-25 00:16:15 -0500

    test

--- server/responses/index.js
+++ server/responses/index.js
@@ -4,7 +4,10 @@ export const updateResponses = (newResponses, updates, userId) => {
 
 	const ready = [];
 	for(const response in updates) {
-		console.log('newResponses[response]', newResponses[response], JSON.stringify(newResponses[response]))
+		console.log('where does it break 0')
+
+		console.log('newResponses[response]', newResponses[response], JSON.stringify(newResponses[response]));
+		console.log('where does it break 1')
 		ready.push(newResponses[response]);
 	}
 
