ca1a7ea51db3dc84ed328f6468a7b6a9f3cc97ba
Ben Efigenio <befigenio@gmail.com>
2020-06-25 00:13:40 -0500

    test

--- server/responses/index.js
+++ server/responses/index.js
@@ -4,6 +4,7 @@ export const updateResponses = (newResponses, updates, userId) => {
 
 	const ready = [];
 	for(const response in updates) {
+		console.log('newResponses[response]', newResponses[response], JSON.stringify(newResponses[response]))
 		ready.push(newResponses[response]);
 	}
 
