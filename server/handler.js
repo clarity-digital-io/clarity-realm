96a56330bc190bd98b5677084cd4473f7adc3644
Ben Efigenio <befigenio@gmail.com>
2020-06-26 15:23:05 -0500

    test

--- server/handler.js
+++ server/handler.js
@@ -1,5 +1,5 @@
 import { updateAnswers } from './answers';
-import { updateResponses, insertResponses } from './responses';
+import { updateResponses, insertResponses, deleteResponses } from './responses';
 
 export const handleChange = async function (changeEvent) {
 	const matches = changeEvent.path.match("^/([^/]+)/([^/]+)$");
@@ -11,7 +11,7 @@ export const handleChange = async function (changeEvent) {
 	if(changeEvent.changes.hasOwnProperty('Response')) {
 
 		if(changeEvent.changes.Response.hasOwnProperty('deletions')) {
-			//deleteResponse will delete answers too
+			deleteResponses(changeEvent.changes.Response.deletions, userId); 
 		}
 
 		if(changeEvent.changes.Response.hasOwnProperty('newModifications')) {
@@ -20,7 +20,6 @@ export const handleChange = async function (changeEvent) {
 		}
 
 		if(changeEvent.changes.Response.hasOwnProperty('insertions')) {
-
 			insertResponses(changeEvent.changes.Response.insertions, userId); 
 		}
 
@@ -44,3 +43,7 @@ export const handleChange = async function (changeEvent) {
 	}
 
 }
+
+00554000005E3mEAAS
+
+a0754000002RgJyAAK
--- server/responses/index.js
+++ server/responses/index.js
@@ -15,31 +15,11 @@ export const updateResponses = (newResponses, updates, userId) => {
 
 export const insertResponses = (inserts, userId) => {
 
-	const ready = [];
-	console.log('insertResponses', JSON.stringify(inserts));
-
 	produce('responses', JSON.stringify(inserts), userId);
-
-	// for(const response in updates) {
-	// 	let sResponse = JSON.stringify(newResponses[response]);
-	// 	let parsedResponse = JSON.parse(sResponse); 
-	// 	ready.push(parsedResponse);
-	// }
-
-	//produce('responses', JSON.stringify(ready), userId);
 
 }
 
-export const deleteResponses = (inserts, userId) => {
-
-	const ready = [];
-	console.log('insertResponses', JSON.stringify(inserts));
-	// for(const response in updates) {
-	// 	let sResponse = JSON.stringify(newResponses[response]);
-	// 	let parsedResponse = JSON.parse(sResponse); 
-	// 	ready.push(parsedResponse);
-	// }
-
-	//produce('responses', JSON.stringify(ready), userId);
-
+export const deleteResponses = (deletions, userId) => {
+	console.log('deletions', deletions); 
+	//produce('delete-responses', JSON.stringify(deletions), userId);
 }
