65b59eba9b1113d3d27c64cce122cc458ff07318
Ben Efigenio <befigenio@gmail.com>
2020-06-26 14:03:16 -0500

    test

--- server/responses/index.js
+++ server/responses/index.js
@@ -17,6 +17,23 @@ export const insertResponses = (inserts, userId) => {
 
 	const ready = [];
 	console.log('insertResponses', JSON.stringify(inserts));
+
+	produce('responses', JSON.stringify(inserts), userId);
+
+	// for(const response in updates) {
+	// 	let sResponse = JSON.stringify(newResponses[response]);
+	// 	let parsedResponse = JSON.parse(sResponse); 
+	// 	ready.push(parsedResponse);
+	// }
+
+	//produce('responses', JSON.stringify(ready), userId);
+
+}
+
+export const deleteResponses = (inserts, userId) => {
+
+	const ready = [];
+	console.log('insertResponses', JSON.stringify(inserts));
 	// for(const response in updates) {
 	// 	let sResponse = JSON.stringify(newResponses[response]);
 	// 	let parsedResponse = JSON.parse(sResponse); 
