31d1e96742baa1c29e6d5acab4b537a5bb48ae78
Ben Efigenio <befigenio@gmail.com>
2020-06-25 20:05:26 -0500

    test

--- server/answers/index.js
+++ server/answers/index.js
@@ -3,12 +3,11 @@ import { produce } from "../queue";
 export const updateAnswers = (newAnswers, updates, userId) => {
 
 	const ready = [];
-	console.log('userId', userId); 
+
 	for(const answer in updates) {
 		let sAnswer = JSON.stringify(newAnswers[answer]); 
 		ready.push(JSON.parse(sAnswer));
 	}
-	console.log('ready', ready); 
 	produce('answers', JSON.stringify(ready), userId);
 
 }
--- server/responses/index.js
+++ server/responses/index.js
@@ -4,14 +4,10 @@ export const updateResponses = (newResponses, updates, userId) => {
 
 	const ready = [];
 	for(const response in updates) {
-		console.log('where does it break 0')
-
-		console.log('newResponses[response]', newResponses[response], JSON.stringify(newResponses[response]));
-		console.log('where does it break 1')
-		ready.push( JSON.stringify(newResponses[response]) );
-		console.log('accessing it again breaks it'); 
+		let sResponse = JSON.stringify(newResponses[response]);
+		ready.push(JSON.parse(sResponse));
 	}
 
-	produce('responses', ready, userId);
+	produce('responses', JSON.stringify(ready), userId);
 
 }
