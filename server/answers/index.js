5fcd5470c6b509c37ddcc83e5c2c59ca0927c78a
Ben Efigenio <befigenio@gmail.com>
2020-06-25 19:19:12 -0500

    test

--- server/answers/index.js
+++ server/answers/index.js
@@ -7,7 +7,7 @@ export const updateAnswers = (newAnswers, updates, userId) => {
 	for(const answer in updates) {
 		ready.push(JSON.stringify(newAnswers[answer]));
 	}
-
+	console.log('ready', ready); 
 	produce('answers', ready, userId);
 
 }
