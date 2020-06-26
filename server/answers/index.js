7347e187fd0f65a4d001c823a380e5c19016c95d
Ben Efigenio <befigenio@gmail.com>
2020-06-25 19:42:59 -0500

    test

--- server/answers/index.js
+++ server/answers/index.js
@@ -5,7 +5,8 @@ export const updateAnswers = (newAnswers, updates, userId) => {
 	const ready = [];
 	console.log('userId', userId); 
 	for(const answer in updates) {
-		ready.push(JSON.stringify(newAnswers[answer]));
+		let sAnswer = JSON.stringify(newAnswers[answer]); 
+		ready.push(JSON.parse(sAnswer));
 	}
 	console.log('ready', ready); 
 	produce('answers', JSON.stringify(ready), userId);
