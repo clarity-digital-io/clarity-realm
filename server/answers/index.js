b120659368f5112557bc8dad2e77794ee2c58628
Ben Efigenio <befigenio@gmail.com>
2020-06-28 00:19:06 -0500

    test

--- server/answers/index.js
+++ server/answers/index.js
@@ -9,6 +9,7 @@ export const updateAnswers = (updates, userId) => {
 }
 
 export const insertAnswers = (inserts, userId) => {
+	console.log('inserts[1].ContentDocument', inserts[1].ContentDocument); 
 	console.log('answers', JSON.stringify(inserts[1].ContentDocument)); 
 
 	if(inserts.length > 0) {
