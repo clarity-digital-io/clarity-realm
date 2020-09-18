780a308129eb1890f5c205dc200e796a1c039122
Ben Efigenio <befigenio@gmail.com>
2020-09-18 12:36:04 -0500

    change handler

--- server/queue/index.js
+++ server/queue/index.js
@@ -5,7 +5,7 @@ let HOST = 'ec2-54-198-67-0.compute-1.amazonaws.com';
 let PASSWORD = 'pa52ef8b5d034514d7e08a40ae7be9213d85032d1255a43b6393b948832e172cd';
 
 export const produce = async (queue, body, userId) => {
-
+	console.log('body', body); 
 	try {
 		let realmQueue = new Queue(queue, {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
 		await realmQueue.add({ body: body, userId: userId, organizationId: '00D2F000000AiZOUA0' });
